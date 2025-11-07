4c8836f6337e40d4e650c7c176e8ab16f47979ea
younus-always <younusofficial443@gmail.com>
2025-11-07 12:28:22 +0600

    form validation using zod, setting up redux for state management

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,15 @@ import './index.css'
 import { RouterProvider } from 'react-router'
 import { router } from './routes'
 import { ThemeProvider } from './providers/theme.provider'
+import { Provider as ReduxProvider } from "react-redux";
+import { store } from './redux/store'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider defaultTheme='system' storageKey='vite-ui-theme'>
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <ReduxProvider store={store}>
+      <ThemeProvider defaultTheme='system' storageKey='vite-ui-theme'>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </ReduxProvider>
   </StrictMode>,
 )
